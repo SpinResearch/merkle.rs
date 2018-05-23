8561d21db29cfe074fb57e01b4e232a88292db10
Ding Xiang Fei <dingxiangfei2009@gmail.com>
2018-05-23 18:25:48 +0800

    Assert protoc version while building with `cargo`

--- build.rs
+++ build.rs
@@ -1,6 +1,26 @@
+#[cfg(feature = "serialization-protobuf")]
 extern crate protoc_rust;
 
-fn build_protobuf<'a>(out_dir: &'a str, input: &'a [&'a str], includes: &'a [&'a str]) {
+#[cfg(feature = "serialization-protobuf")]
+fn assert_protobuf_version(version: &str) {
+    use std::process::{Command, Stdio};
+    let protoc = Command::new("protoc")
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .args(&["--version"])
+        .spawn()
+        .unwrap();
+    let version_output = protoc.wait_with_output().unwrap();
+    assert!(version_output.status.success());
+    assert_eq!(
+        String::from_utf8(version_output.stdout).unwrap().trim(),
+        version
+    );
+}
+
+#[cfg(feature = "serialization-protobuf")]
+fn build_protobuf(out_dir: &str, input: &[&str], includes: &[&str]) {
     use self::protoc_rust::{run, Args};
     run(Args {
         out_dir,
@@ -9,7 +29,13 @@ fn build_protobuf<'a>(out_dir: &'a str, input: &'a [&'a str], includes: &'a [&'a
     }).expect("protoc");
 }
 
+#[cfg(feature = "serialization-protobuf")]
+fn build_protobuf_schemata() {
+    assert_protobuf_version("libprotoc 3.5.1");
+    build_protobuf("src/proto", &["protobuf/proof.proto"], &[]);
+}
+
 fn main() {
     #[cfg(feature = "serialization-protobuf")]
-    build_protobuf("src/proto", &["protobuf/proof.proto"], &[]);
+    build_protobuf_schemata();
 }
